bc04eddd334399f5427a1a226d1be5b604e09476
TChallala <89186499+TChallala@users.noreply.github.com>
2021-12-16 01:24:43 +0800

    Create act.c

---
+++ act.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    int odd1 = 3;
+    int odd2 = 1;
+    int even = 2;
+    printf("input any positive integers: ");
+    scanf("%d", &num);
+
+    while(num > 1) {
+        if(num % 2 == 0) {
+            num = num / even;
+            printf("\nCurrrent integer value is: %d", num);
+        }
+        else{
+        num = num * odd1 + odd2;
+        printf("\nCurrrent integer value is: %d\n", num);
+        }
+        }
+        return 0;
+    }
